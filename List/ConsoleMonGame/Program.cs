28ee00af1bf0a4c1ea85c7ae27aa7ce640af313a
Clement <35793@ma-web.nl>
2023-05-12 10:18:39 +0200

    Added List Opdtacht

---
+++ List/ConsoleMonGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleMonGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleMon caster = new ConsoleMon(100, 50, "Charizard");
+            ConsoleMon target = new ConsoleMon(100, 50, "Chimchar");
+
+            Skill BurningAttack = new Skill(20, 50, "Burning Attack");
+
+            BurningAttack.UseOn(target, caster);
+        }
+    }
+}
---
+++ ListOpdracht/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<string> birds = new List<string>() {"uil", "kraai", "hond", "papegaai"};
+            List<string> birds2 = new List<string>() {"meeuw", "duif"};
+
+            birds.AddRange(birds2);
+            birds.RemoveAt(2);
+
+            for (int i = 0; i < birds.Count; i++)
+            {
+                string bird = birds[i];
+                Console.WriteLine(bird);
+            }
+        }
+    }
+}
