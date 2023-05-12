59eb2f456093584661ced81f30bffccb744af940
Clement <35793@ma-web.nl>
2023-05-12 09:56:39 +0200

    Classes & Functions opdracht

---
+++ ConsoleMonGame/ConsoleMon.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleMonGame
+{
+    internal class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+        public ConsoleMon(int health, int energy, string name)
+        {
+            this.health = health;
+            this.energy = energy;
+            this.name = name;
+        }
+
+        internal void TakeDamage(int damage)
+        {
+            this.health -= damage;
+        }
+
+        internal void DelpleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+}
--- ConsoleMonGame/Program.cs
+++ ConsoleMonGame/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace ConsoleMonGame
+﻿using System;
+
+namespace ConsoleMonGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConsoleMon caster = new ConsoleMon(100, 50, "Charizard");
+            ConsoleMon target = new ConsoleMon(100, 50, "Chimchar");
+
+            Skill BurningAttack = new Skill(20, 50, "Burning Attack");
+
+            BurningAttack.UseOn(target, caster);
         }
     }
 }
