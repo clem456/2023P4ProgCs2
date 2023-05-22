20addb7d0435b99611dbf1aee9390035e3d7b2ee
Clement <114576100+clem456@users.noreply.github.com>
2023-05-22 11:55:12 +0200

    FileOpdracht Afgemaakt

---
+++ FileDirOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FileDirOpdracht;
+using System.IO;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DirectoryInfo dir = new DirectoryInfo(@"C:\Users\clemc\OneDrive\Documenten\Ma\Lj1\Prog\Periode4\2023P4ProgCs2");
+
+        FileInfo[] fileInfo = dir.GetFiles();
+        DirectoryInfo[] dirInfo = dir.GetDirectories();
+
+        foreach (var file in fileInfo)
+        {
+            Console.WriteLine(file.Name);
+        }
+
+        foreach (var newDir in dirInfo) 
+        {
+            Console.WriteLine(newDir.Name);
+        }
+    }
+}
