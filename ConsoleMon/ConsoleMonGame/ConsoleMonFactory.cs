ecfd1b684e1d661c321776a3419cb260d78a6204
Clement <114576100+clem456@users.noreply.github.com>
2023-06-30 10:11:15 +0200

    Opdracht 16 af

--- ConsoleMon/ConsoleMonGame/ConsoleMonFactory.cs
+++ ConsoleMon/ConsoleMonGame/ConsoleMonFactory.cs
@@ -23,13 +23,33 @@ namespace ConsoleMonGame
             }
         }
 
-        internal void LoadJson(string dataFile)
+        internal List<ConsoleMon> LoadJson(string dataFile)
         {
             string json = File.ReadAllText(dataFile);
 
             List<ConsoleMon> templates = JsonSerializer.Deserialize<List<ConsoleMon>>(json);
 
-            Console.WriteLine(templates[0].name);
+            return templates;
+        }
+
+        internal ConsoleMon CopyConsoleMon(ConsoleMon copyFrom)
+        {
+            ConsoleMon copyResult = new ConsoleMon(copyFrom.health, copyFrom.energy, copyFrom.name, copyFrom.weakness);
+            copyResult.skills = new List<Skill>();
+
+            foreach(Skill skill in copyFrom.skills)
+            {
+                copyResult.skills.Add(skill);
+            }
+
+            return copyResult;
+        }
+
+        internal Skill CopySkill(Skill copyFrom)
+        {
+            Skill copyResult = new Skill(copyFrom.damage, copyFrom.energyCost, copyFrom.name, copyFrom.element);
+
+            return copyResult;
         }
     }
 }
--- ConsoleMon/ConsoleMonGame/Program.cs
+++ ConsoleMon/ConsoleMonGame/Program.cs
@@ -29,6 +29,7 @@ namespace ConsoleMonGame
             TestFactoryFunctions();
             */
 
+            /*
             static void TestConstructors()
             {
                 Console.WriteLine("TestConstructors");
@@ -47,6 +48,49 @@ namespace ConsoleMonGame
             }
 
             TestConstructors();
+            */
+
+            /*
+            static void TestCopySkill()
+            {
+                Console.WriteLine("TestCopySkill");
+                ConsoleMonFactory factory = new ConsoleMonFactory();
+                List<ConsoleMon> templates = factory.LoadJson("monsterdata.json");
+                Skill copyFrom = templates[0].skills[0];
+
+                Skill copy = factory.CopySkill(copyFrom);
+                Console.WriteLine(copy.name == copyFrom.name);
+                Console.WriteLine(copy.damage == copyFrom.damage);
+                Console.WriteLine(copy.element == copyFrom.element);
+                copy.name = "anders";
+                Console.WriteLine(copy.name != copyFrom.name);
+            }
+
+            TestCopySkill();
+            */
+
+            static void TestCopyConsoleMon()
+            {
+                Console.WriteLine("TestCopyConsoleMon");
+                ConsoleMonFactory factory = new ConsoleMonFactory();
+                List<ConsoleMon> templates = factory.LoadJson("monsterdata.json");
+                ConsoleMon copyFrom = templates[0];
+
+                ConsoleMon copy = factory.CopyConsoleMon(copyFrom);
+                Console.WriteLine(copy.name == copyFrom.name);
+                Console.WriteLine(copy.health == copyFrom.health);
+                Console.WriteLine(copy.skills == copyFrom.skills);
+                Console.WriteLine(copy.skills[0] == copyFrom.skills[0]);
+                copy.name = "anders";
+                copy.skills[0].name = "newskill";
+                Console.WriteLine(copy.name != copyFrom.name);
+                Console.WriteLine(copy.skills[0].name != copyFrom.skills[0].name);
+
+                // 1ste false komt omdat het 2 verschillende instanties zijn van skill
+                // 2de false komt omdat de skills wel gelijk zijn aan elkaar maar er staan != voor
+            }
+
+            TestCopyConsoleMon();
         }
     }
 }
